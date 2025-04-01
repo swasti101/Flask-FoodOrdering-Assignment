67013ddddc6d4afecc7509363453397715cb46ad
ConsultAdd <consultadd@ConsultAdds-MacBook-Pro.local>
2025-04-01 13:19:52 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_migrate import Migrate
 
 
